f42679f99e2e046b5a3389b0adfef66033284f3e
Rolando <rolscg@gmail.com>
2026-06-27 01:25:05 +0200

    feat(faults): add deterministic frame-drop injector

---
+++ ros2_ws/src/fault_injector_node/setup.py
@@ -0,0 +1,30 @@
+from glob import glob
+import os
+
+from setuptools import find_packages, setup
+
+
+package_name = "fault_injector_node"
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Rolando",
+    maintainer_email="rolscg@gmail.com",
+    description="Deterministic runtime fault injection nodes for maritime perception replay.",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "fault_injector_node = fault_injector_node.node:main",
+        ],
+    },
+)
